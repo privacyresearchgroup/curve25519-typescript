290df13cf2c9aec608b05e71a0860a05c90cd0ac
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-02 12:07:08 -0500

    remove unused exports

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source ~/dev/tools/emsdk/emsdk_env.sh
+# make sure emcc is in your PATH, e.g. source ~/dev/tools/emsdk/emsdk_env.sh
 emcc native/ed25519/additions/*.c native/curve25519-donna.c native/ed25519/*.c native/ed25519/sha512/sha2big.c \
 -O1 -Qunused-arguments \
 -o src/built/curveasm.js \
 -Inative/ed25519/nacl_includes -Inative/ed25519 -Inative/ed25519/sha512 \
 -s WASM=0 -s MODULARIZE \
--s EXPORTED_FUNCTIONS="['_curve25519_donna','_curve25519_sign','_curve25519_verify','_crypto_sign_ed25519_ref10_ge_scalarmult_base', '_sph_sha512_init', '_malloc']"
+-s EXPORTED_FUNCTIONS="['_curve25519_donna','_curve25519_sign','_curve25519_verify','_malloc']"
 
